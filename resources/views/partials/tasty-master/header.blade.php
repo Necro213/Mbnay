8579442934f3759947f79b95bd22c7ef10aef48e
Alexis <necro213a@gmail.com>
2018-10-06 11:18:36 -0600

    Feat: Se agregaron los layouts correspondientes, faltando unicamente 1

---
+++ resources/views/partials/tasty-master/header.blade.php
@@ -0,0 +1,45 @@
+<nav class="fh5co-nav" role="navigation">
+    <!-- <div class="top-menu"> -->
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 text-center logo-wrap">
+                <div id="fh5co-logo"><a href="index.html">Tasty<span>.</span></a></div>
+            </div>
+            <div class="col-xs-12 text-center menu-1 menu-wrap">
+                <ul>
+                    <li class="active"><a href="index.html">Home</a></li>
+                    <li><a href="menu.html">Menu</a></li>
+                    <li class="has-dropdown">
+                        <a href="gallery.html">Gallery</a>
+                        <ul class="dropdown">
+                            <li><a href="#">Events</a></li>
+                            <li><a href="#">Food</a></li>
+                            <li><a href="#">Coffees</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="reservation.html">Reservation</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+    <!-- </div> -->
+</nav>
+
+<header id="fh5co-header" class="fh5co-cover js-fullheight" role="banner" style="background-image: url(/img/tasty-master/hero_1.jpeg);" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="display-t js-fullheight">
+                    <div class="display-tc js-fullheight animate-box" data-animate-effect="fadeIn">
+                        <h1>The Best Coffee <em>&amp;</em> Restaurant <em>in</em> Brooklyn</h1>
+                        <h2>Brought to you by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
